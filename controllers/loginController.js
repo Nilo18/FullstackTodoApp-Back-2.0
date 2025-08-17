a9a8a2c0a7fffa0d45a7ef519c1807279a324f91
Nilo18 <nikalongurashvili@gmail.com>
2025-08-17 16:02:32 +0400

    login debug

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -6,16 +6,24 @@ async function loginUser(req, res, next) {
     try {
         const {username} = req.body;
         const userPassword = req.body.password
+        console.log('Username: ', username)
+        console.log('Password: ', userPassword)
         const compareUsername = await User.findOne({username})
+        console.log('Compared username: ', compareUsername)
         if (!compareUsername) {
             return res.status(404).send("User not found.")
         }
         const comparePassword = await bcrypt.compare(password, userPassword)
+        console.log('Compared password: ', comparePassword)
         if (!comparePassword) {
             return res.status(401).send("Please enter a valid password.")
         }
         const accessToken = createAccessToken(username);
+        console.log('Access Token:', accessToken)
+
         const refreshToken = createRefreshToken(username);
+        console.log('Refresh Token:', refreshToken)
+
         res.status(200).cookie('refreshToken', refreshToken, {
             httpOnly: true,
             secure: true,
