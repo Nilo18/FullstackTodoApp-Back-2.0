d18acbe4ec0bc1fccbb93c68755cc017900667a5
Nilo18 <nikalongurashvili@gmail.com>
2025-08-19 16:17:42 +0400

    Added debug logs

--- controllers/jwtRefreshController.js
+++ controllers/jwtRefreshController.js
@@ -8,11 +8,14 @@ function resendAccessToken(req, res, next) {
         return
     }
     const payload = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET)
+    console.log('The payload is: ', payload)
     const payLoadUserId = payload.userId
+    console.log('Received the userId: ', payLoadUserId)
     const {username} = req.body
+    console.log('Sent by: ', username)
     const newAccessToken = jwt.sign({payLoadUserId, username}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '15m'})
-    console.log(newAccessToken)
-    console.log(req.userId) // Delete this log later when comfortable
+    console.log('New access token is: ', newAccessToken)
+    // console.log(req.userId) // Delete this log later when comfortable
     res.status(200).json(newAccessToken)
     next()
 }
