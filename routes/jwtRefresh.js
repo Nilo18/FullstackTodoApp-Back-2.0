e476f7ae8c77fce9ac0043fd3db575aa42efe992
Nilo18 <nikalongurashvili@gmail.com>
2025-08-19 15:41:28 +0400

    test

--- routes/jwtRefresh.js
+++ routes/jwtRefresh.js
@@ -3,6 +3,7 @@ const jwtRefreshRouter = express.Router()
 const { resendAccessToken } = require('../controllers/jwtRefreshController.js')
 const { authenticate } = require('../middleware/jwtAuthChecker.js')
 
-jwtRefreshRouter.post('/', authenticate, resendAccessToken)
+// Maybe come back here if some issues arise and add authenticate back
+jwtRefreshRouter.post('/', resendAccessToken)
 
 module.exports = jwtRefreshRouter
