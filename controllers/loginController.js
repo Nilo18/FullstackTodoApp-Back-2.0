7d6779ea01fd5304b07ec91b9ab1eb0287e45159
Nilo18 <nikalongurashvili@gmail.com>
2025-08-21 23:12:18 +0400

    More minor changes

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -9,12 +9,12 @@ async function loginUser(req, res, next) {
         const userPassword = req.body.password
         const findByUsername = await User.findOne({username}) // Find the user by the username in mongodb
         if (!findByUsername) {
-            return res.status(404).send("User not found.")
+            return res.status(404).send({error: "User not found."})
         }
         // Compare the given password and the matching user's password using bcrypt
         const comparePassword = await bcrypt.compare(userPassword, findByUsername.password) 
         if (!comparePassword) {
-            return res.status(401).send("Please enter the correct password.")
+            return res.status(401).send({error: "Please enter the correct password."})
         }
         const accessToken = createAccessToken(findByUsername.userId, username);
 
