0aac8e0ceb28927e9f06ad54b5d0199b251ce54b
Nilo18 <nikalongurashvili@gmail.com>
2025-08-28 04:11:02 +0400

    Added more debug logs

--- middleware/deadlineNotifier.js
+++ middleware/deadlineNotifier.js
@@ -8,22 +8,25 @@ async function checkTaskExpiry() {
         const upcoming = new Date(Date.now() + 24 * 60 * 60 * 1000)
 
         const expiringTasks = await Task.find({deadline: {$gte: now, $lte: upcoming}, notified: false})
+        console.log('The tasks about to expire: ', expiringTasks)
         // Step 1: Group tasks by userId
         const tasksByUser = {};
         expiringTasks.forEach(task => {
-        if (!tasksByUser[task.userId]) tasksByUser[task.userId] = [];
-        tasksByUser[task.userId].push(task);
+            if (!tasksByUser[task.userId]) tasksByUser[task.userId] = [];
+            tasksByUser[task.userId].push(task);
         });
+        console.log('taskByUser: ', tasksByUser)
 
         // Step 2: Send one email per user
         for (const userId in tasksByUser) {
             const tasks = tasksByUser[userId];
             const user = await User.findOne({ userId });
+            console.log('The found user is: ', user)
 
             // Build a message listing all tasks
             const taskList = tasks.map(t => `- ${t.taskName}`).join('\n');
             const message = `Following tasks have less than 24 hours until they expire:\n${taskList}`;
-            console.log(message)
+            console.log('The message is: ', message)
 
             // Send a single email
             await sendEmailToNotify(user.email, 'Tasks are expiring', message, 'gmail');
